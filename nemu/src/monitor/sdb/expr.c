c514b5fc95411a0f824b2837e9a25b85f7439160
tracer-ics2022 <tracer@njuics.org>
2023-06-02 21:13:22 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:13:22 up 2 days,  5:24,  2 users,  load average: 0.51, 0.61, 0.54

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -154,16 +154,17 @@ bool check_parentheses(int p, int q){
   printf("l=%d, r=%d\n", l, r);
   char *stack = (char *)malloc((l-r+1)*sizeof(char));
   int top = -1;
-  if(tokens[l].type=='(') stack[++top] = '(';
 
-  // while(l<r){
-  //   if(tokens[l].type=='(') stack[++top] = '(';
-  //   // else if(tokens[l].type==')') {
-  //   //   if(stack[top] == ')') top--;
-  //   //   else return false;
-  //   // }
-  //   l++;
-  // }
+
+  while(l<r){
+    if(tokens[l].type=='(') stack[++top] = '(';
+    else if(tokens[l].type==')') {
+      if(stack[top] == ')') top--;
+      else return false;
+    }
+    l++;
+    printf("l=%d\n",l);
+  }
   if(top == -1) return true;
   else return false;
 }

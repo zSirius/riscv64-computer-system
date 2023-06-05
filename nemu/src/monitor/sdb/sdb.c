a79379ce8f87961ef90248ceaffc74db00222e68
tracer-ics2022 <tracer@njuics.org>
2023-06-05 14:44:38 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:44:38 up 3 days,  7:29,  5 users,  load average: 0.55, 1.03, 1.27

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -84,7 +84,7 @@ static int cmd_x(char *args){
   bool success;
 
   sscanf(args, "%d %s", &N, str);
-  printf("%d, %s", N, str);
+  printf("%d, %s\n", N, str);
   EXPR_vddr = expr(str, &success);
 
   if(!success){

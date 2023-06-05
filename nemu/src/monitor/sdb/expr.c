060e06840ef5075f8f8f90889fff1f209ae21ed7
tracer-ics2022 <tracer@njuics.org>
2023-06-05 10:41:25 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:41:25 up 3 days,  5:07,  5 users,  load average: 0.21, 0.20, 0.23

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -216,7 +216,8 @@ word_t expr(char *e, bool *success) {
 
   for(int i=0; i<nr_token; i++){
     //check TK_DEREF
-    if(tokens[i].type == '*' && (i==0 || (tokens[i-1].type != TK_NUM && tokens[i-1].type != ')'))){
+    if(tokens[i].type == '*' && (i==0 || 
+      (tokens[i-1].type != TK_NUM && tokens[i-1].type != ')' && tokens[i-1].type != TK_HEX && tokens[i-1].type != TK_REG))){
       tokens[i].type = TK_DEREF;
     }
     //check register name

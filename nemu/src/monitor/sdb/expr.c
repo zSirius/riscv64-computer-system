da2b98fee74c0d116d7018290849c8c7eafd21ec
tracer-ics2022 <tracer@njuics.org>
2023-06-05 11:04:51 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:04:51 up 3 days,  5:31,  5 users,  load average: 0.25, 0.25, 0.26

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -342,7 +342,7 @@ word_t eval(int p, int q, bool *success){
 
 void test(){
   bool success;
-  word_t val = expr("$ra + (((6)+58*((77))*65)/(71)*$ra)", &success);
+  word_t val = expr("$ra + (((6)+58*((77))*65)/(71*$ra))", &success);
   // printf("---cnts of tokens:%d---\n", nr_token);
   // for(int i=0; i<nr_token; i++){
   //   printf("idx is %d, token type:%d, str:%s\n", i, tokens[i].type, tokens[i].str);

0e6c6929f41d4e4600c0084791fc49878b39262f
tracer-ics2022 <tracer@njuics.org>
2023-06-22 14:00:11 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:00:11 up 6 days,  9:40,  4 users,  load average: 0.15, 0.21, 0.29

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,52 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+void itoa(char str[], int d){
+  size_t num_len = 0;
+  int t = d;
+  while(t != 0){
+    num_len++;
+    t /= 10;
+  }
+  for(int i = num_len-1; i>=0; i--){
+    str[i] = d%10 + '0';
+    d /= 10;
+  }
+  str[num_len] = '\0';
+}
+
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  size_t i=0;
+  int d;
+  char *s;
+
+  va_start(ap,fmt);
+  while(*fmt){
+    switch (*fmt){
+    case '%':
+      fmt++;
+      if(*fmt == 'd'){
+        char str[64];
+        d = va_arg(ap, int);
+        itoa(str, d);
+        strcpy(out+i, str);
+        i += strlen(str);
+      }else if(*fmt == 's'){
+        s = va_arg(ap, char *);
+        strcpy(out+i, s);
+        i += strlen(s);
+      }
+      fmt++;
+      break;
+    default:
+      *(out+(i++)) = *fmt++;
+      break;
+    }
+  }
+  *(out+i) = '\0';
+  va_end(ap);
+  return strlen(out);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {

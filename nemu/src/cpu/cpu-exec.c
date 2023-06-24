174502c189a91bd67bc1a6b9e288f5af1f2e3b73
tracer-ics2022 <tracer@njuics.org>
2023-06-24 15:25:33 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:25:33 up 6 days, 14:45,  4 users,  load average: 0.40, 0.30, 0.26

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -47,8 +47,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
   s->snpc = pc;
-  isa_exec_once(s);
+  isa_exec_once(s); //change: s->snpc += 4;
   cpu.pc = s->dnpc;
+  printf("cpu.pc = 0x%016lx , s->pc = 0x%016lx \n", cpu.pc, s->pc);
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);

ebe56a666a4b578322ff6200953419a82b758cba
tracer-ics2022 <tracer@njuics.org>
2023-06-24 21:28:22 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:28:22 up 6 days, 19:52,  4 users,  load average: 0.75, 0.53, 0.51

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -63,9 +63,9 @@ void mtrace_display(){
     printf("Never read memory!\n");
   }else{
     printf("Read memory history:\n");
-    printf("     addr       len/bytes\n");
+    printf("  addr  len(bytes)\n");
     for(int i=_mtrace.read_start; i!=_mtrace.read_end; i = (i+1)%64){
-      printf("0x%08x  %d\n", _mtrace.read_mtrace[i].addr, _mtrace.read_mtrace[i].len);
+      printf("0x%08x      %d\n", _mtrace.read_mtrace[i].addr, _mtrace.read_mtrace[i].len);
     }
   }
 
@@ -73,9 +73,9 @@ void mtrace_display(){
     printf("Never wrote to memory!\n");
   }else{
     printf("Write memory history:\n");
-    printf("     addr       len/bytes\n");
+    printf("  addr  len(bytes)\n");
     for(int i=_mtrace.write_start; i!=_mtrace.write_end; i = (i+1)%64){
-      printf("0x%08x    %d\n", _mtrace.write_mtrace[i].addr, _mtrace.write_mtrace[i].len);
+      printf("0x%08x      %d\n", _mtrace.write_mtrace[i].addr, _mtrace.write_mtrace[i].len);
     }
   }
 }

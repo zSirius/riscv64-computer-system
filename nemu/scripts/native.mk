74226edbbf336f89e4d5f322c8df5b135daacb55
tracer-ics2022 <tracer@njuics.org>
2023-06-27 20:34:40 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:34:40 up 8 days,  7:14,  4 users,  load average: 0.10, 0.16, 0.17

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -28,6 +28,7 @@ override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt
 override ARGS += $(ARGS_DIFF)
 
 # Command to execute NEMU
+$(info this is native) #调试
 IMG ?=
 NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 

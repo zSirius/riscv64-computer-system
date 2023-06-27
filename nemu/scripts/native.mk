3c9d1e2cc55fc0c6d61ed505c32a250ba0222cfe
tracer-ics2022 <tracer@njuics.org>
2023-06-27 20:42:32 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:42:32 up 8 days,  7:21,  4 users,  load average: 0.42, 0.27, 0.21

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -34,9 +34,9 @@ NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 
 run-env: $(BINARY) $(DIFF_REF_SO)
 
-run: run-env
-	$(call git_commit, "run NEMU")
-	$(NEMU_EXEC)
+# run: run-env
+# 	$(call git_commit, "run NEMU")
+# 	$(NEMU_EXEC)
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")

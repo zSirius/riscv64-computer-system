a19dedae02e73a59aa0a9083f248f196454adc68
tracer-ics2022 <tracer@njuics.org>
2023-06-28 19:20:28 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:20:28 up 8 days, 21:03,  6 users,  load average: 0.46, 0.52, 0.47

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -32,7 +32,7 @@ void get_shstrtab(FILE *elf_fp){
     get_shoff(elf_fp);
 
 
-    SET_FP(shoff+e_shstrndx*64);
+    SET_FP(2276);
     byte_read = fread(&shstrndx_off, sizeof(shstrndx_off), 1, elf_fp);
 
     if(byte_read!=0)

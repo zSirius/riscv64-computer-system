d6ff6a7a5bfbe0f616c7496fe8fd4de98cf400da
tracer-ics2022 <tracer@njuics.org>
2023-06-28 16:55:33 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:55:33 up 8 days, 18:38,  6 users,  load average: 0.58, 0.55, 0.43

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -1,5 +1,13 @@
 #include <common.h>
 
+#define RESET_FP fseek(elf_fp, 0, SEEK_SET);
+
+typedef struct{
+    char name[16];
+    vaddr_t start;
+    size_t size;
+}func_table;
+
 void init_elf(const char *elf_file){
     if(elf_file == NULL) return;
     printf("elf_file = %s\n",elf_file);
@@ -9,9 +17,12 @@ void init_elf(const char *elf_file){
     Assert(fp, "Can not open '%s'", elf_file);
     elf_fp= fp;
 
-    unsigned char byte[3];
-    size_t byte_read = fread(&byte, sizeof(unsigned char), 3, elf_fp);
+    uint64_t s;
+    size_t byte_read = fread(&s, sizeof(unsigned char), 8, elf_fp);
     if(byte_read != 0)
-        printf("%x %x %x\n", byte[0], byte[1],byte[2]);
+        printf("%lux\n", s);
+
+
+
 
 }

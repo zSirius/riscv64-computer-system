2750239daf906c2bad139ae6b73c05ab0e96ac7f
tracer-ics2022 <tracer@njuics.org>
2023-06-28 18:33:12 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:33:12 up 8 days, 20:16,  6 users,  load average: 0.40, 0.40, 0.33

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -28,10 +28,10 @@ void init_elf(const char *elf_file){
     Assert(fp, "Can not open '%s'", elf_file);
     elf_fp= fp;
 
-    uint64_t s;
-    size_t byte_read = fread(&s, sizeof(unsigned char), 8, elf_fp);
+    uint64_t s[8];
+    size_t byte_read = fread(&s, sizeof(uint64_t), 8, elf_fp);
     if(byte_read != 0)
-        printf("%lx\n", s);
+        printf("%lx\n", s[7]);
 
     get_shstrtab(elf_fp);
 

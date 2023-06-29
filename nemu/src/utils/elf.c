920466b0247ee5c5026668490fd23c7ae2145edd
tracer-ics2022 <tracer@njuics.org>
2023-06-29 09:05:54 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:05:54 up 9 days,  2:31,  4 users,  load average: 0.36, 0.44, 0.53

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -186,21 +186,7 @@ void get_shstrtab(FILE *elf_fp){
     for(int i=0; i<ftab_num; i++)
         printf("%s, 0x%lx, %lu\n", ftab[i].name, ftab[i].start, ftab[i].size);
 
-    
-    
-    
-    // SET_FP(shoff+64*2);
-    // uint32_t name;
-    // byte_read = fread(&name, sizeof(uint32_t), 1 , elf_fp);
-    // if(byte_read != 0)
-    //     printf("name = %x", name);
-    // uint64_t a[8];
-    // byte_read = fread(a, sizeof(uint64_t), 8 , elf_fp);
-    // if(byte_read != 0)
-    //     for(int i=0; i<8; i++)
-    //         printf("%lx \n", a[i]);
     return;
-
 }
 
 
@@ -213,15 +199,6 @@ void init_elf(const char *elf_file){
     Assert(fp, "Can not open '%s'", elf_file);
     elf_fp= fp;
 
-    // uint64_t s[8];
-    // size_t byte_read = fread(s, sizeof(uint64_t), 8, elf_fp);
-    // if(byte_read != 0)
-    //     for(int i=0; i<8; i++)
-    //         printf("%lx\n", s[i]);
-
     get_shstrtab(elf_fp);
-
-
-
 
 }

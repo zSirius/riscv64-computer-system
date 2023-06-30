468087d5049a739f73fe86fd5c065088b3a76a59
tracer-ics2022 <tracer@njuics.org>
2023-06-30 16:14:24 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:14:24 up 9 days, 19:52,  4 users,  load average: 1.07, 0.61, 0.34

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,13 +4,13 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  int i;
-  uint32_t vgactl = inl(VGACTL_ADDR);
-  int w = vgactl & 0xffff;  
-  int h = vgactl >> 16;  
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
+  // int i;
+  // uint32_t vgactl = inl(VGACTL_ADDR);
+  // int w = vgactl & 0xffff;  
+  // int h = vgactl >> 16;  
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {

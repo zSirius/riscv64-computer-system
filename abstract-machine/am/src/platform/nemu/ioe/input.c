4cf87afaf4e1cf71443300d4e3840d8ad8325ac6
tracer-ics2022 <tracer@njuics.org>
2023-06-30 11:47:11 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:47:11 up 9 days, 15:25,  4 users,  load average: 0.83, 1.41, 0.91

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -6,5 +6,5 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t scancode = inl(KBD_ADDR);
   kbd->keydown = scancode | KEYDOWN_MASK;
-  kbd->keycode = scancode & 0xff;
+  kbd->keycode = scancode & (~KEYDOWN_MASK);
 }

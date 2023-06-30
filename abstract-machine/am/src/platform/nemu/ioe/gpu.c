93f24e4db815a565744772cff24542911e2c07a3
tracer-ics2022 <tracer@njuics.org>
2023-06-30 18:28:01 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:28:01 up 9 days, 22:06,  4 users,  load average: 0.36, 0.30, 0.23

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,13 +4,6 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  // int i;
-  // uint32_t vgactl = inl(VGACTL_ADDR);
-  // int w = vgactl & 0xffff;  
-  // int h = vgactl >> 16;  
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < w * h; i ++) fb[i] = i;
-  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -25,6 +18,15 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)FB_ADDR;
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+  for(int i=y; i<y+h; i++){
+    for(int j=x; j<x+w; j++){
+      fb[i * screen_w + j] = pixels[(i-y) * w + (j-x)];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }

367b570a1893bd353bf47e50ab6bc3226e2f9f17
tracer-ics2022 <tracer@njuics.org>
2023-07-02 14:45:19 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:45:19 up 10 days, 18:51,  4 users,  load average: 0.58, 0.40, 0.30

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: printf("this is EVENT_yield!\n");
+    case EVENT_YIELD: printf("this is EVENT_yield!\n");break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 

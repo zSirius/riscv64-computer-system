c213a6b42f26d24a24dcca4a9bfcfda6a553cfb2
tracer-ics2022 <tracer@njuics.org>
2023-07-03 14:27:39 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:27:39 up 11 days,  7:37,  4 users,  load average: 0.61, 0.38, 0.28

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,19 @@
 #include <common.h>
 #include "syscall.h"
+
+#define RET(ret) c->GPRx = ret;
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+
 
   switch (a[0]) {
-    case SYS_yield: yield(); break;
+    case SYS_exit: halt(a[1]); break;
+    case SYS_yield: yield(); RET(0); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }

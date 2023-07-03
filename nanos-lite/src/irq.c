e6d727eec80defe8a8494d0637249f0126445ca4
tracer-ics2022 <tracer@njuics.org>
2023-07-03 14:10:55 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:10:55 up 11 days,  7:21,  4 users,  load average: 0.38, 0.26, 0.21

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,6 +4,7 @@
 #define INC_PC c->mepc += 4
 
 static Context* do_event(Event e, Context* c) {
+  printf("EVENT NO: %d\n", e.event);
   switch (e.event) {
     case EVENT_SYSCALL: do_syscall(c); INC_PC; break;
     case EVENT_YIELD: printf("this is EVENT_yield!\n"); INC_PC;break;

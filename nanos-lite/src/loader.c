b93b711ed98f9ddc70427e8d3086049e93a3ab0f
tracer-ics2022 <tracer@njuics.org>
2023-07-04 21:22:00 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:00 up 12 days,  6:08,  4 users,  load average: 0.72, 0.54, 0.49

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -17,6 +17,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   Elf64_Ehdr elf_header;
   fs_read(fd, &elf_header, sizeof(elf_header));
+  printf("elf_header.e_ident=%d\n",*(uint32_t *)elf_header.e_ident);
   //ramdisk_read(&elf_header, 0, sizeof(elf_header));
   assert(*(uint32_t *)elf_header.e_ident == 0x464c457f);
   assert(elf_header.e_machine == EM_RISCV);

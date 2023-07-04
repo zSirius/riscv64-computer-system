8ce91ec81678c633a5cf34aa654cdbc297f6b43b
tracer-ics2022 <tracer@njuics.org>
2023-07-04 20:51:08 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:51:08 up 12 days,  5:37,  4 users,  load average: 1.08, 0.76, 0.62

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -10,6 +10,7 @@ int main() {
   
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
+  printf("size = %d\n", size);
   assert(size == 5000);
 
   //fseek(fp, 500 * 5, SEEK_SET);
@@ -17,14 +18,13 @@ int main() {
   long size2 = ftell(fp);
   //assert(size == 2500);
  
-  int i, n;
 
+  // int i, n;
+  // for (i = 500; i < 1000; i ++) {
+  //   fscanf(fp, "%d", &n);
+  //   // assert(n == i + 1);
+  // }
 
-  for (i = 500; i < 1000; i ++) {
-    fscanf(fp, "%d", &n);
-    // assert(n == i + 1);
-  }
-  
   // printf("\n");
   // printf("n=%d\n",n);
 

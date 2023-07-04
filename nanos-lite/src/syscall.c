42d6b0de88697d852fe09b7b19d462f8fb04a5b3
tracer-ics2022 <tracer@njuics.org>
2023-07-04 13:25:28 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:25:28 up 11 days, 22:11,  4 users,  load average: 0.98, 0.56, 0.43

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,13 +7,13 @@
 int write(int fd, const void *buf, size_t count);
 
 void do_syscall(Context *c) {
-  printf("this is do_syscall\n");
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
 
+  printf("this is do_syscall, call num = %d\n", (int)a[0]);
   int ret;
 
   switch (a[0]) {

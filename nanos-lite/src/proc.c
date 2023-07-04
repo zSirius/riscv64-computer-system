b1c3202f9bae0fea7b523b120f886e115a24808d
tracer-ics2022 <tracer@njuics.org>
2023-07-04 11:53:45 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:53:45 up 11 days, 20:39,  4 users,  load average: 0.88, 0.70, 0.61

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,7 +22,7 @@ void hello_fun(void *arg) {
 void naive_uload(PCB *pcb, const char *filename);
 
 void init_proc() {
-  naive_uload(NULL, "/bin/hello");
+  naive_uload(NULL, "/bin/file-test");
   switch_boot_pcb();
 
   Log("Initializing processes...");

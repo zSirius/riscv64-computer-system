307c2dc0afd16ad8299af95b640bcddff02a9b4a
tracer-ics2022 <tracer@njuics.org>
2023-07-06 23:25:34 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:25:34 up 12 days, 21:00,  4 users,  load average: 0.65, 0.61, 0.48

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <sys/_timeval.h>
 
+uint32_t NDL_GetTicks();
 int main(){
     uint32_t oldTime, curTime;
     oldTime = NDL_GetTicks();
     while(1){
         curTime = NDL_GetTicks();
         if(curTime - oldTime >=500){
-            printf("msec = %ld. Time has gone 5 seconds.\n", oldTime.tv_sec);
+            printf("msec = %d. Time has gone 5 seconds.\n", oldTime);
             oldTime = curTime;
         }
     }

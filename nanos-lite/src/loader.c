3bdeea85fc3a1725eaa33eba204403fbce847bf0
tracer-ics2022 <tracer@njuics.org>
2023-07-06 18:47:43 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:47:43 up 12 days, 16:23,  4 users,  load average: 0.93, 0.61, 0.37

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -17,7 +17,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   Elf64_Ehdr elf_header;
   fs_read(fd, &elf_header, sizeof(elf_header));
-  printf("elf_header.e_ident=%d\n",*(uint32_t *)elf_header.e_ident);
   //ramdisk_read(&elf_header, 0, sizeof(elf_header));
   assert(*(uint32_t *)elf_header.e_ident == 0x464c457f);
   assert(elf_header.e_machine == EM_RISCV);
@@ -28,12 +27,10 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     fs_read(fd, &ph_entry, sizeof(ph_entry));
     //ramdisk_read(&ph_entry, elf_header.e_phoff + i * elf_header.e_phentsize, sizeof(ph_entry));
     if(ph_entry.p_type == PT_LOAD){
-      printf("this is LOAD\n");
       fs_lseek(fd, ph_entry.p_offset, SEEK_SET);
       fs_read(fd, (void *)ph_entry.p_vaddr, ph_entry.p_memsz);
       //ramdisk_read((void *)ph_entry.p_vaddr, ph_entry.p_offset, ph_entry.p_memsz);
       memset((void *)(ph_entry.p_vaddr + ph_entry.p_filesz), 0, ph_entry.p_memsz - ph_entry.p_filesz);
-      printf("end load\n");
     }
   }
 

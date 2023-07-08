d094b7868d46147a78dc25d085746d292adb4eec
tracer-ics2022 <tracer@njuics.org>
2023-07-08 12:01:35 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:01:35 up 13 days, 13:34,  4 users,  load average: 0.87, 0.80, 0.67

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -9,12 +9,12 @@ int main() {
   NDL_Init(0);
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
-  // assert(bmp);
-  // NDL_OpenCanvas(&w, &h);
-  // NDL_DrawRect(bmp, 0, 0, w, h);
-  // free(bmp);
-  // NDL_Quit();
-  // printf("Test ends! Spinning...\n");
-  // while (1);
+  assert(bmp);
+  NDL_OpenCanvas(&w, &h);
+  NDL_DrawRect(bmp, 0, 0, w, h);
+  free(bmp);
+  NDL_Quit();
+  printf("Test ends! Spinning...\n");
+  while (1);
   return 0;
 }

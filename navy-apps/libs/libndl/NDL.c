336ccea26f251f7971686534719441f61ed2be5d
tracer-ics2022 <tracer@njuics.org>
2023-07-30 20:00:45 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:00:45 up  2:52,  1 user,  load average: 1.06, 0.90, 0.75

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -64,6 +64,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   int width, height;
   _read(5 ,buf, sizeof(buf));
   sscanf(buf, "[WIDTH]:%d [HEIGHT]:%d/n", &width, &height);
+  printf("x=%d, y=%d, w=%d, h=%d, width=%d, height=%d\n",x,y,w,h,width,height);
   //for(int i=0; i<h; i++){
   for(int i=0; i<1; i++){ //debug
     printf("(y+i)*width = %d\n",(y+i)*width);

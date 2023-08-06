4927acbcfea585f1a5204e10153917b7b0bdddd6
tracer-ics2022 <tracer@njuics.org>
2023-08-06 19:34:05 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:34:05 up 22:39,  1 user,  load average: 1.37, 0.76, 0.79

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -64,7 +64,7 @@ static void prev() {
 }
 
 static void clear_display(void) {
-  SDL_FillRect(screen, NULL, 0x007acc);
+  SDL_FillRect(screen, NULL, 0xffffff);
 }
 
 int main(int argc, char *argv[], char *envp[]) {
--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -5,28 +5,33 @@
 #include <stdlib.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
-  // assert(dst && src);
-  // assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
-  // int16_t src_x, src_y, dst_x, dst_y;
-  // uint16_t w, h;
-  // if(srcrect == NULL){
-  //   src_x = 0, src_y = 0;
-  //   w = src->w, h = src->h;
-  // }else{
-  //   src_x = srcrect->x, src_y = srcrect->y;
-  //   w = srcrect->w, h = srcrect->h;
-  // }
+  assert(dst && src);
+  assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
+  int16_t src_x, src_y, dst_x, dst_y;
+  uint16_t w, h;
+  if(srcrect == NULL){
+    src_x = 0, src_y = 0;
+    w = src->w, h = src->h;
+  }else{
+    src_x = srcrect->x, src_y = srcrect->y;
+    w = srcrect->w, h = srcrect->h;
+  }
 
-  // if(dstrect == NULL){
-  //   dst_x = 0, dst_y = 0;
-  // }else{
-  //   dst_x = dstrect->x, dst_y = dstrect->y;
-  //   //ignore dstrect`s w and h;
-  // }
+  if(dstrect == NULL){
+    dst_x = 0, dst_y = 0;
+  }else{
+    dst_x = dstrect->x, dst_y = dstrect->y;
+    //ignore dstrect`s w and h;
+  }
 
-  // //init offset in bytes
+  printf("src_x=%d, src_y=%d, dst_x = %d, dst_y = %d, w=%d, h=%d\n", src_x, src_y,dst_x, dst_y,w,h);
+
+  //init offset in bytes
   // int src_init_off = (src_y*src->w + src_x) * src->pitch / src->w;
   // int dst_init_off = (dst_y*dst->w + dst_x) * dst->pitch / dst->w;
+
+  int src_init_off = (src_y*src->w + src_x);
+  int dst_init_off = (dst_y*dst->w + dst_x);
 
   // for(int i=0; i<h; i++){
   //   for(int j=0; j<(w * src->pitch / src->w); j++){
@@ -34,6 +39,13 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   //     *(src->pixels + src_init_off + i * src->pitch + j * src->pitch / src->w);
   //   }
   // }
+
+  for(int i=0; i<h; i++){
+    for(int j=0; j<w; j++){
+      *(uint32_t *)(dst->pixels + (dst_init_off + i * dst->w + j) * (dst->pitch/dst->w)) = 
+      *(uint32_t *)(src->pixels + (src_init_off + i * src->w + j) * (src->pitch/src->w));
+    }
+  }
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {

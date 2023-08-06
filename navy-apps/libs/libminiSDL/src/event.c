041d901a7e8028a011870eebc083fd092026c15d
tracer-ics2022 <tracer@njuics.org>
2023-08-06 14:41:51 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:41:51 up 18:26,  1 user,  load average: 1.53, 1.74, 1.26

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -30,6 +30,7 @@ int SDL_WaitEvent(SDL_Event *event) {
     for(int i=0; ; i++){
       if(strcnmp(buf+3, keyname[i], strlen(buf)-3-1) == 0){
         event->key.keysym.sym = i;
+        printf("keycode=%d\n", i);
         break;
       }
     }

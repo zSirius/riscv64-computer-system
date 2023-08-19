771b729121315458c145f05b34a94c57550ec353
tracer-ics2022 <tracer@njuics.org>
2023-08-19 15:09:03 +0800

    >  compile NEMU SA22225532 庄金凯 Linux sirius-virtual-machine 5.19.0-42-generic #43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:09:03 up 1 day,  6:54,  1 user,  load average: 2.14, 2.25, 2.56

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -78,7 +78,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
-  printf("In SDL_UpdateRect\n");
+  //printf("In SDL_UpdateRect\n");
   if(x==0 && y==0 && w==0 && h==0){
     // char buf[64];
     // int screen_width, screen_height;
